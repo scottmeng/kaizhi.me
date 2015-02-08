9a44a4918be06a48738b116b876ab81d73b22265
scottmeng <blackridermkz@gmail.com>
2015-02-09 00:25:43 +0800

    update router, add draft

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var expressLayouts = require('express-ejs-layouts');
+var fs = require('fs');
 
 var routes = require('./routes/index');
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var posts = require('../data/posts.json');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -12,6 +13,24 @@ router.get('/thoughts', function(req, res) {
 	res.render('thoughts-list', {
 		page: 'thoughts'
 	});
+});
+
+router.get('/thoughts/:slug', function(req, res) {
+	var slug = req.params.slug;
+
+	var targetPost = posts.some(function(post) {
+		return !!(post.slug === slug);
+	});
+
+	if (targetPost) {
+		res.render('thoughts-list', {
+			page: 'thoughts'
+		});
+	} else {
+		res.render('error', {
+			page: 'about'
+		});
+	}
 });
 
 /* GET other thoughts pages */
