18dfc93b25fe5d73b47f2fff3d9226c170911fd2
scottmeng <blackridermkz@gmail.com>
2016-04-19 01:18:59 +0800

    add new section

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var posts = require('../data/posts.json');
+var randoms = require('../data/randoms.json');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -13,6 +14,21 @@ router.get('/thoughts', function(req, res) {
 	res.render('thoughts-list', {
 		page: 'thoughts',
 		posts: posts
+	});
+});
+
+router.get('/sevens-and-eights', function(req, res) {
+	res.render('sevens-and-eights', {
+		page: 'seven-and-eights',
+		posts: randoms
+	});
+});
+
+router.get('/bibi/:slug', function(req, res) {
+	var slug = req.params.slug;
+
+	var targetBibi = bibis.some(function(bibi) {
+		return bibi.slug !== slug;
 	});
 });
 
